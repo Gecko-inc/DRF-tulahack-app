38104026003b86ed70363c2aa011df2b7d721717
Nicstim <56487341+nicstim@users.noreply.github.com>
2021-09-15 02:57:33 +0300

    fix

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
 # Файл для тестовых функций
-import pyqiwi
-
-token = "984765f1efad088e7b0a61d35afa20a9"
-
-qiwi = pyqiwi.Wallet(token, number="79813514374", contract_info=True, auth_info=True, user_info=True)
-
-# print(pyqiwi.generate_form_link("99", "79813514374", 1.1, comment="123"))
-for item in qiwi.history().get('transactions'):
-    if item.comment == "Да" and item.sum.amount >= 1.11:
-        print(True)
+import this
