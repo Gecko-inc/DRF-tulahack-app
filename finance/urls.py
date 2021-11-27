af225c26c983d7e52fb4a901b0505835bee0ab43
nicstim <sashapskov60@gmail.com>
2021-11-28 01:12:05 +0300

    icon svg

--- finance/urls.py
+++ finance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ExpensesView, CategoryView
+from .views import ExpensesView, CategoryView, IncomeView
 
 app_name = "finance"
 
 urlpatterns = [
     path("expenses/", ExpensesView.as_view(), name="Expenses"),
+    path("income/", IncomeView.as_view(), name="Income"),
     path("category/", CategoryView.as_view(), name="category")
 ]
--- finance/views.py
+++ finance/views.py
@@ -128,12 +128,46 @@ class CategoryView(APIView):
                              required=['title'],
                              properties={
                                  'title': openapi.Schema(type=openapi.TYPE_STRING),
+                                 "icon": openapi.Schema(type=openapi.TYPE_INTEGER)
                              },
                          ),
                          )
     def post(self, request):
+        icon_dict = {
+            1: "fa-solid_seedling.svg",
+            2: "fa-solid_spa.svg",
+            3: "jam_baidu.svg",
+            4: "maki_animal-shelter.svg",
+            5: "svg/bone.svg",
+            6: "svg/flower.svg",
+            7: "svg/joystick.svg",
+            8: "svg/dices.svg",
+            9: "fa-solid_dumbbell.svg",
+            10: "fa-solid_basketball-ball.svg",
+            11: "fa-solid_paint-brush.svg",
+            12: "fa-solid_drum.svg",
+            13: "fa-solid_guitar.svg",
+            14: "eva_speaker-fill.svg",
+            15: "maki_bowling-alley.svg",
+            16: "zondicons_artist.svg",
+            17: "maki_attraction.svg",
+            18: "eva_film-fill.svg",
+            19: "jam_pictures-f.svg",
+            20: "fa-solid_icons.svg",
+            21: "fa-solid_apple-alt.svg",
+            22: "maki_restaurant-pizza.svg",
+            23: "fa-solid_capsules.svg",
+            24: "maki_defibrillator.svg",
+            25: "maki_grocery.svg",
+            26: "fa-solid_gas-pump.svg",
+            27: "zondicons_key.svg",
+            28: "jam_rocket-f.svg",
+            29: "zondicons_airplane.svg",
+            30: "zondicons_location-hotel.svg"
+        }
         user = init_user(request)
-        category = Category.objects.create(title=request.data.get("title"), user=user)
+        category = Category.objects.create(title=request.data.get("title"), user=user,
+                                           icon=f"/static/{icon_dict.get(request.data.get('key'))}")
         return Response(CategorySerializer(category, many=False).data, status=200)
 
     @swagger_auto_schema(tags=['Finance'],
