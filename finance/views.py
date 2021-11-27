448ddcc77e199f8fc926fc616504cc1e80ae4b18
nicstim <sashapskov60@gmail.com>
2021-11-27 17:13:00 +0300

    PUT expenses view

--- finance/views.py
+++ finance/views.py
@@ -28,7 +28,6 @@ class ExpensesView(APIView):
                          )
     @transaction.atomic
     def post(self, request):
-        sid = transaction.savepoint()
         user = request.user
         data = request.data
         try:
@@ -52,3 +51,45 @@ class ExpensesView(APIView):
             transaction.savepoint_rollback(sid)
             return Response({"error": "the balance cannot be negative"}, status=400)
         return Response(status=201)
+
+    @swagger_auto_schema(tags=['Finance'],
+                         request_body=openapi.Schema(
+                             type=openapi.TYPE_OBJECT,
+                             required=['expense_id'],
+                             properties={
+                                 'expense_id': openapi.Schema(type=openapi.TYPE_INTEGER)
+                             },
+                         ),
+                         )
+    def delete(self, request, **kwargs):
+        try:
+            Expenses.objects.get(id=request.data.get("expense_id")).delete()
+        except Expenses.DoesNotExist:
+            pass
+        return Response(status=201)
+
+    @swagger_auto_schema(tags=['Finance'],
+                         request_body=openapi.Schema(
+                             type=openapi.TYPE_OBJECT,
+                             required=['id'],
+                             properties={
+                                 'id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                                 'title': openapi.Schema(type=openapi.TYPE_STRING),
+                                 'category_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                                 'money': openapi.Schema(type=openapi.TYPE_STRING),
+                             },
+                         ),
+                         )
+    def put(self, request):
+        data = request.data
+        try:
+            expenses = Expenses.objects.get(id=data.get("id"))
+            expenses.title = data.get("title", expenses.title)
+            expenses.money = data.get("money", expenses.money)
+            expenses.category_id = data.get("category_id", expenses.category_id)
+            expenses.save()
+            return Response(ExpensesSerializer(expenses, many=False).data, status=200)
+        except Expenses.DoesNotExist:
+            return Response({
+                "error": "Expenses does not exist"
+            }, status=404)
